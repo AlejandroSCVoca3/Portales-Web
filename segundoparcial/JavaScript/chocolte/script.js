e94dd2bac72d0e697b350097dea1034dfb2fc2c8
Alejandro Sanchez <sanchezconsuelojosealejandro@gmail.com>
2025-10-21 19:44:20 -0600

    Delete segundoparcial/JavaScript/chocolte directory

--- segundoparcial/JavaScript/chocolte/script.js
+++
@@ -1,8 +0,0 @@
-// Chocolatería - JS básico para scroll o futura funcionalidad
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log("🍫 La Dulcería del Cacao — JS listo");
-
-  // Puedes agregar animaciones, validaciones o listeners aquí.
-  // Por ejemplo: resaltar menú según scroll.
-});
